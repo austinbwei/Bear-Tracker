4d8337227db45e0091b6d89748582db4572aa0f1
aweiler97 <aweiler@unca.edu>
2019-11-18 16:17:56 -0500

    Iteration 3 - Delete geofence if new one is created within it

--- app/src/main/java/edu/csci373/unca/BearActivity.java
+++ app/src/main/java/edu/csci373/unca/BearActivity.java
@@ -21,8 +21,12 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import java.util.List;
 import java.util.Random;
 
 public class BearActivity extends AppCompatActivity {
@@ -32,6 +36,7 @@ public class BearActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST = 1;
     private FusedLocationProviderClient client;
     private FirebaseFirestore db;
+    private CollectionReference mFences;
     private AlarmManager mAlarmManager;
     private Button mAddFence;
     private Button mShowMap;
@@ -43,6 +48,7 @@ public class BearActivity extends AppCompatActivity {
 
         client = LocationServices.getFusedLocationProviderClient(this);
         db = FirebaseFirestore.getInstance();
+        mFences = db.collection("geofences");
 
         mAddFence = (Button) findViewById(R.id.bear_spotted);
         mAddFence.setOnClickListener(new View.OnClickListener() {
@@ -96,39 +102,73 @@ public class BearActivity extends AppCompatActivity {
                     double lat = location.getLatitude();
                     double lon = location.getLongitude();
 
-                    GeoFence geoFence = new GeoFence();
+                    final GeoFence geoFence = new GeoFence();
                     geoFence.setLat(lat);
                     geoFence.setLon(lon);
                     geoFence.setRadius(DEFAULT_RADIUS);
 
                     Log.d(TAG, "Bear spotted at Latitude: " + lat + " Longitude: " + lon);
 
-                    newFenceRef.set(geoFence).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    // Check if geofence is within another geofence
+                    mFences.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                         @Override
-                        public void onComplete(@NonNull Task<Void> task) {
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
                             if (task.isSuccessful()) {
-                                Toast.makeText(BearActivity.this, R.string.success_toast, Toast.LENGTH_SHORT).show();
+                                List<DocumentSnapshot> docList = task.getResult().getDocuments();
+                                for (DocumentSnapshot doc : docList) {
+                                    double lat = doc.getDouble("lat");
+                                    double lon = doc.getDouble("lon");
+                                    double radius = doc.getDouble("radius");
 
-                                // Schedule recurring alarm to increase geofence radius and eventually delete
-                                mAlarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-                                Intent intent = new Intent(BearActivity.this, FirebaseAlarm.class);
+                                    Location oldLocation = new Location("");
+                                    oldLocation.setLatitude(lat);
+                                    oldLocation.setLongitude(lon);
 
-                                final int MIN = 0;
-                                final int MAX = 1000;
-                                int alarmID = new Random().nextInt((MAX - MIN) + 1) + MIN;      // Generate random ID number
+                                    Location newLocation = new Location("");
+                                    newLocation.setLatitude(geoFence.getLat());
+                                    newLocation.setLongitude(geoFence.getLon());
 
-                                intent.putExtra("documentID", newFenceRef.getId());
-                                intent.putExtra("alarmID", alarmID);
+                                    float distance = newLocation.distanceTo(oldLocation);
 
-                                PendingIntent pendingIntent = PendingIntent.getBroadcast(BearActivity.this, alarmID, intent, 0);
-                                mAlarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                                        SystemClock.elapsedRealtime(),
-                                        15 * 60 * 1000,     // Every 15min
-                                        pendingIntent);
+                                    // Is within already existing geofence
+                                    // Delete original geofence and add new geofence
+                                    if (distance <= radius) {
+                                        Log.d(TAG, "Deleting document " + doc.getId());
+                                        mFences.document(doc.getId()).delete();
+                                    }
+                                    Log.d(TAG, "Geofence at Latitude: " + lat + " Longitude: " + lon);
+                                }
 
-                                Log.d(TAG, "Passed alarm for Document: " + newFenceRef.getId());
-                            } else {
-                                Toast.makeText(BearActivity.this, R.string.fail_toast, Toast.LENGTH_SHORT).show();
+                                // Add new geofence
+                                newFenceRef.set(geoFence).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(BearActivity.this, R.string.success_toast, Toast.LENGTH_SHORT).show();
+
+                                            // Schedule recurring alarm to increase geofence radius and eventually delete
+                                            mAlarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                                            Intent intent = new Intent(BearActivity.this, FirebaseAlarm.class);
+
+                                            final int MIN = 0;
+                                            final int MAX = 1000;
+                                            int alarmID = new Random().nextInt((MAX - MIN) + 1) + MIN;      // Generate random ID number
+
+                                            intent.putExtra("documentID", newFenceRef.getId());
+                                            intent.putExtra("alarmID", alarmID);
+
+                                            PendingIntent pendingIntent = PendingIntent.getBroadcast(BearActivity.this, alarmID, intent, 0);
+                                            mAlarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                                                    SystemClock.elapsedRealtime(),
+                                                    15 * 60 * 1000,     // Every 15min
+                                                    pendingIntent);
+
+                                            Log.d(TAG, "Passed alarm for Document: " + newFenceRef.getId());
+                                        } else {
+                                            Toast.makeText(BearActivity.this, R.string.fail_toast, Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
                             }
                         }
                     });
